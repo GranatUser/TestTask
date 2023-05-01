be304854cd747e5d2c854eea5b2fde8526be9da9
GranatUser <112390294+GranatUser@users.noreply.github.com>
2023-05-01 18:52:00 +0000

    Deploying to gh-pages from @ GranatUser/TestTask@7e4df2593c5cd612d9d581e4fc787c385e6600e4 🚀

---
+++ static/js/902.956f3fc8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[902],{2902:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(7689),u=r(2791),a=r(184);function c(){var e=(0,n.s0)();return(0,u.useEffect)((function(){e("/",{replace:!0})}),[e]),(0,a.jsx)(a.Fragment,{})}}}]);
+//# sourceMappingURL=902.956f3fc8.chunk.js.map
